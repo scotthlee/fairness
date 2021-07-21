fixed FPR sign flipping

dev.py
@@ -19,8 +19,9 @@ pm = tools.ProbabilityMatrices(y, y_, a)
 # Getting the pairwise constraints
 tpr_cons, fpr_cons, norm_cons = tools.constraint_pairs(pm)
 
-# Writing out some different loss functions;
-# I think these are basically correct, but I'm not 100% sure
+# Writing out some different loss functions; micro is what Hardt et al.
+# use in the 2016 paper, but it forces everything to the majority class;
+# 'macro' seems to work much better
 macro = -1 * pm.cp_mats.flatten()
 w_macro = np.tile(np.repeat(y_p, 3), 3) * macro
 micro = pm.loss_weights
@@ -36,6 +37,9 @@ tpr_eq_opt = linprog(c=macro,
                      bounds=[0, 1],
                      method='highs')
 tpr_eq_mats = tools.pars_to_cpmat(tpr_eq_opt)
+tpr_eq_rocs = tools.parmat_to_roc(tpr_eq_mats,
+                                  pm.p_vecs,
+                                  pm.cp_mats)
 
 # And then relaxing the constraints a bit
 tpr_ineq_bounds = np.repeat(.1, 6)
@@ -47,9 +51,12 @@ tpr_ineq_opt = linprog(c=macro,
                        bounds=[0, 1],
                        method='highs')
 tpr_ineq_mats = tools.pars_to_cpmat(tpr_ineq_opt)
+tpr_ineq_rocs = tools.parmat_to_roc(tpr_ineq_mats,
+                                    pm.p_vecs,
+                                    pm.cp_mats)
 
-# Trying with equalized odds
-eo_bounds = np.repeat(.01, 12)
+# Trying with equalized odds; first with the relaxed constraints
+eo_bounds = np.repeat(.05, 12)
 eo_con = np.concatenate([tpr_cons, fpr_cons])
 eo_opt = linprog(c=macro,
                  A_ub=eo_con,
@@ -59,7 +66,11 @@ eo_opt = linprog(c=macro,
                  bounds=[0, 1],
                  method='highs')
 eo_mats = tools.pars_to_cpmat(eo_opt)
+eo_rocs = tools.parmat_to_roc(eo_mats,
+                              pm.p_vecs,
+                              pm.cp_mats)
 
+# And now with the strict constraints
 eo_eq_bounds = np.repeat(0, 12)
 eo_eq_opt = linprog(c=macro,
                     A_eq=np.concatenate([eo_con, norm_cons]),
@@ -67,3 +78,6 @@ eo_eq_opt = linprog(c=macro,
                     bounds=[0, 1],
                     method='highs')
 eo_eq_mats = tools.pars_to_cpmat(eo_eq_opt)
+eo_eq_rocs = tools.parmat_to_roc(eo_eq_mats,
+                              pm.p_vecs,
+                              pm.cp_mats)

tools.py
@@ -944,15 +944,16 @@ def constraint_weights(p_vec, cp_mat):
     tpr = np.zeros(shape=(n_classes, n_params))
     fpr = np.zeros(shape=(n_classes, n_params))
     
-    # Getting the combinations of outcomes
-    combos = list(combinations(range(n_classes), n_classes - 1))
-    combos = [list(c) for c in combos]
-    
     # Getting weights for the first n-1 classes
-    for i, c in enumerate(combos):
+    for i in range(n_classes):
+        # Dropping row to calculate FPR
+        p_i = np.delete(p, i)
+        M_i = np.delete(M, i, 0)
+        print((p_i, M_i))
+        
         start = i * (n_classes)
         end = start + n_classes
-        fpr[i, start:end] = np.dot(p[c], M[c]) / p[c].sum()
+        fpr[i, start:end] = np.dot(p_i, M_i) / p_i.sum()
         tpr[i, start:end] = M[i]
     '''
     # Getting the weights for the last class in terms of the others
@@ -1029,12 +1030,13 @@ def constraint_pairs(pms):
         tpr_flip = np.sign(np.diag(diffs)).reshape(-1, 1)
         fpr_flip = np.sign(np.sum(fprs[c[0]], 1) - np.sum(fprs[c[1]], 1))
         fpr_flip = fpr_flip.reshape(-1, 1)
+        fpr_flip[np.where(fpr_flip == 0)] = 1
         
         # Filling in the constraints
         tpr_cons[i, c[0]] = np.multiply(tpr_flip, tprs[c[0]])
         tpr_cons[i, c[1]] = np.multiply(tpr_flip, -1 * tprs[c[1]])
-        fpr_cons[i, c[0]] = fpr_flip * fprs[c[0]]
-        fpr_cons[i, c[1]] = fpr_flip * -1 * fprs[c[1]]
+        fpr_cons[i, c[0]] = np.multiply(fpr_flip, fprs[c[0]])
+        fpr_cons[i, c[1]] = np.multiply(fpr_flip, -1 * fprs[c[1]])
     
     # Filling in the norm constraints
     one_cons = np.zeros(shape=(n_groups * n_classes,