80b37515724834843ce246b9a416a9c28037184a
Scott Lee <yle4@cdc.gov>
2021-09-30 09:05:15 -0400

    adding first version of test_run()

--- exp_test.py
+++ exp_test.py
@@ -4,24 +4,59 @@ import pandas as pd
 from balancers import MulticlassBalancer
 import tools
 
-y_test = tools.simulate_y([1, 2, 3], 
-                          ['a', 'b', 'c'],
-                          [.3, .7], 
-                          [[.1, .1, .8], 
-                           [.3, .5, .2]],
-                          n=10000)
-p_y_a = np.array([[[.1, .2, .7], 
+
+def test_run(outcomes,
+             groups,
+             p_group,
+             p_y_group,
+             p_yh_group,
+             loss='micro',
+             goal='odds'):
+    # Simulating the input data
+    y_test = tools.simulate_y(outcomes,
+                              groups,
+                              p_group,
+                              p_y_group)
+    yh_test = tools.simulate_yh(y_test,
+                                p_yh_group,
+                                outcomes)
+    
+    # Setting up the variables
+    y = y_test.y.values
+    a = y_test.group.values
+    yh = y_test.y_hat.values
+    
+    # Running the optimizations
+    b = MulticlassBalancer(y, yh, a)
+    b.adjust(loss=loss, goal=goal)
+    accuracy = b.opt.fun
+    status = b.opt.status
+    roc = b.rocs[0]
+    
+    # Bundling things up
+    out = {
+           'goal': goal,
+           'loss': loss,
+           'status': status,
+           'accuracy': accuracy,
+           'roc': roc
+    }
+    
+    return out
+
+outcomes = [1, 2, 3]
+groups = ['a', 'b', 'c']
+p_group = [.3, .7]
+p_y_group = [[.1, .1, .8],
+             [.3, .5, .2]]
+p_yh_group = np.array([[[.1, .2, .7], 
                    [.6, .3, .1], 
                    [.1, .1, .8]],
                   [[.8, .1, .1], 
                    [.2, .5, .3], 
                    [.2, .2, .6]]])
-yh_test = tools.simulate_yh(test_df=y_test, 
-                            p_y_a=p_y_a, 
-                            outcomes=[1, 2, 3])
-
-
-group_ids = [np.where(yh_test.group == g)[0] 
-             for g in yh_test.group.unique()]
-cp_mats = [tools.cp_mat(yh_test.y[ids], yh_test.y_hat[ids])
-           for ids in group_ids]
+test = test_run(outcomes,
+                groups,
+                p_group,
+                p_y_group,
+                p_yh_group)
