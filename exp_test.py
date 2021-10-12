6f17577d2e503bb9cc5ff45359e056aa9bb64195
Scott Lee <yle4@cdc.gov>
2021-10-12 11:57:14 -0400

    adding 3-group 3-class experiments

--- exp_test.py
+++ exp_test.py
@@ -92,23 +92,118 @@ stats_23 = pd.concat([r['stats'] for r in res], axis=0)
 stats_23.to_csv('2-group 3-class stats.csv', index=False)
 
 # Setting up a 3-group 3-group problem
-p33 = {'outcomes': {
-            'balance': [[.333, .333, .334],
-                        [.333, .333, .334],
-                [.333, .333, .334]],
-    'equal_imbalance': [[.1, .5, .4],
-                        [.1, .5, .4],
-                        [.1, .5, .4]],
-    'unequal_imbalance': [[.1, .5, .4],
-                          [.5, .4, .1],
-                          [.5, .4, .1]]
-    },
-          'groups': {
-               'no_minority': [.333, .333, .334],
-               'one_slight_minority': [.2, .4, .4],
-               'two_slight_minorities': [.2, .6, .2],
-               'two_strong_minorities': [.1, .8, .1],
-               'one_strong_minority': [.1, .5, .4]
-          }
-    }
+bias_types = ['low_one', 'medium_one', 'high_one',
+              'low_two', 'medium_two', 'high_two']
+out_types = ['balanced', 'one_rare', 'two_rare']
+group_types = ['no_minority', 'one_slight_minority', 'one_strong_minority',
+               'two_slight_minorities', 'two_strong_minorities']
+
+sits = [[[[g, o, p] for p in bias_types]
+             for o in out_types]
+            for g in group_types]
+sits = tools.flatten([l for l in tools.flatten(sits)])
+
+p33 = {'groups': {
+        'no_minority': np.array([.33, .33, .34]),
+        'one_slight_minority': np.array([.4, .4, .2]),
+        'one_strong_minority': np.array([.45, .45, .1]),
+        'two_slight_minorities': np.array([.6, .2, .2]),
+        'two_strong_minorities': np.array([.8, .1, .1])},
+       'outcomes': {
+           'balanced': np.array([[.333, .333, .334],
+                                [.333, .333, .334],
+                                [.333, .333, .334]]),
+           'one_rare': np.array([[.1, .5, .4],
+                                 [.1, .5, .4],
+                                 [.1, .5, .4]]),
+           'two_rare': np.array([[.1, .8, .1],
+                                 [.1, .8, .1],
+                                 [.1, .8, .1]]),
+           },
+       'bias': {
+           'low_one': [np.array([[.9, .05, .05],
+                                 [.05, .9, .05],
+                                 [.05, .05, .9]]),
+                       np.array([[.9, .05, .05],
+                                 [.05, .9, .05],
+                                 [.05, .05, .9]]),
+                        np.array([[.8, .1, .1],
+                                  [.1, .8, .1],
+                                  [.1, .1, .8]])],
+           'medium_one': [np.array([[.9, .05, .05],
+                                   [.05, .9, .05],
+                                   [.05, .05, .9]]),
+                          np.array([[.9, .05, .05],
+                                    [.05, .9, .05],
+                                    [.05, .05, .9]]),
+                        np.array([[.7, .15, .15],
+                                  [.15, .7, .15],
+                                  [.15, .15, .7]])],
+           'high_one': [np.array([[.9, .05, .05],
+                                   [.05, .9, .05],
+                                   [.05, .05, .9]]),
+                        np.array([[.9, .05, .05],
+                                  [.05, .9, .05],
+                                  [.05, .05, .9]]),
+                         np.array([[.5, .25, .25],
+                                   [.25, .5, .25],
+                                   [.25, .25, .5]])],
+             'low_two': [np.array([[.9, .05, .05],
+                                   [.05, .9, .05],
+                                   [.05, .05, .9]]),
+                         np.array([[.8, .1, .1],
+                                   [.1, .8, .1],
+                                   [.1, .1, .8]]),
+                          np.array([[.8, .1, .1],
+                                    [.1, .8, .1],
+                                    [.1, .1, .8]])],
+             'medium_two': [np.array([[.9, .05, .05],
+                                     [.05, .9, .05],
+                                     [.05, .05, .9]]),
+                            np.array([[.7, .15, .15],
+                                      [.15, .7, .15],
+                                      [.15, .15, .7]]),
+                          np.array([[.7, .15, .15],
+                                    [.15, .7, .15],
+                                    [.15, .15, .7]])],
+             'high_two': [np.array([[.9, .05, .05],
+                                     [.05, .9, .05],
+                                     [.05, .05, .9]]),
+                          np.array([[.5, .25, .25],
+                                    [.25, .5, .25],
+                                    [.25, .25, .5]]),
+                           np.array([[.5, .25, .25],
+                                     [.25, .5, .25],
+                                     [.25, .25, .5]])]
+           }
+       }
+
+input_33 = [[[(p33['groups'][g], p33['outcomes'][o], p33['bias'][b]) 
+        for b in bias_types] 
+       for o in out_types] 
+      for g in group_types]
+input_33 = tools.flatten([l for l in tools.flatten(input_33)])
+
+# Running the sim
+with Pool() as p:
+    input = [[[(outcomes, 
+                groups3, 
+                t[0], 
+                t[1], 
+                t[2], 
+                loss, 
+                goal,
+                sits[i][0],
+                sits[i][1],
+                sits[i][2])
+            for i, t in enumerate(input_33)]
+           for loss in losses]
+          for goal in goals]
+    input = tools.flatten([l for l in tools.flatten(input)])
+    res = p.starmap(tools.test_run, input)
+    p.close()
+    p.join()
+
+stats_33 = pd.concat([r['stats'] for r in res], axis=0)
+stats_33.to_csv('3-group 3-class stats.csv', index=False)
 
