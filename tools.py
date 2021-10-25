fde5275e99503e9e34e4e2c7ba591380094b50e8
Scott Lee <yle4@cdc.gov>
2021-10-25 08:35:22 -0400

    deleting some print statements

--- tools.py
+++ tools.py
@@ -95,7 +95,7 @@ def test_run(outcomes,
     
     # Running the optimizations
     b = balancers.MulticlassBalancer(y, yh, a)
-    b.adjust(loss=loss, goal=goal)
+    b.adjust_new(loss=loss, goal=goal)
     status = b.opt.status
     old_acc = np.dot(b.p_y, np.diag(b.cp_mat))
     if status == 0:
@@ -1108,14 +1108,12 @@ def parmat_to_roc(par_mats,
     '''Takes the tensor of parameters from the LP and gets sensitivity and 
     specificity for each group.
     '''
-    print(par_mats.shape)
     n_groups = len(p_vecs)
     n_classes = len(p_vecs[0])
     rocs = np.zeros(shape=(n_groups, n_classes, 2))
     for i, par_mat in enumerate(par_mats):
         p = p_vecs[i]
         M = cp_mats[i]
-        print(M.shape, par_mat.shape)
         rocs[i, :, 1] = np.diag(np.dot(M, par_mat))
         for j in range(n_classes):
             weights = np.dot(np.delete(p, j), np.delete(M, j, 0))
@@ -1146,10 +1144,12 @@ def sparsify(col, reshape=True, return_df=True, long_names=False):
     if return_df:
         out = pd.DataFrame(out, columns=columns)
     return out
-<<<<<<< HEAD
 
 
 def otsu(scores, cutpoints, labels, qcut=True):
+    '''Uses Otsu thresholding/Jenk's method to find optimal cutpoints
+    for turning continuous variables into multiclass variables.
+    '''
     if np.min(cutpoints) != 0:
         cutpoints = [0] + [c for c in cutpoints] + [1]
     if qcut:
@@ -1206,6 +1206,3 @@ def cp_mat_summary(b, slim=True, title=None, round=2):
         out.append(df)
     
     return pd.concat(out, axis=1)
-=======
-    
->>>>>>> a3fec37... fixed reshaping bug in adjust_new
