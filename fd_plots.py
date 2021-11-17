400a559a53d6dc5bc94fabaef61ef2bee7555a7d
Scott Lee <yle4@cdc.gov>
2021-11-17 16:45:21 -0500

    image refresh

--- fd_plots.py
+++ fd_plots.py
@@ -24,16 +24,17 @@ weed_probs = pickle.load(open(data_dir + 'weed_probs.pkl', 'rb'))
 obesity_probs = pickle.load(open(data_dir + 'obesity_probs.pkl', 'rb'))
 park_probs = pickle.load(open(data_dir + 'park_probs.pkl', 'rb'))
 
-df_names = ['bar', 'cannabis', 'obesity', 'parkinsons']
+df_names = ['Bar', 'Drugs', 'Obesity', 'Parkinsons']
 datasets = [bar, weed, obesity, park]
 probs = [bar_probs, weed_probs, obesity_probs, park_probs]
 
 # Defining the 4 fairness types
 goals = ['odds', 'strict', 'opportunity', 'demographic_parity']
+goal_names = ['Odds', 'Strict', 'Opportunity', 'Parity']
 
 # Making the two relplots: one for micro loss, and one for macro
 sns.set_style('darkgrid')
-sns.set(font_scale=.75)
+sns.set(font_scale=1)
 
 fig, axes = plt.subplots(nrows=4, ncols=4, sharey=False)
 
@@ -52,7 +53,7 @@ for i, goal in enumerate(goals):
                       ax=axes[i, j])
 
 # Labeling the fairness constraint types
-for i, goal in enumerate(goals):
+for i, goal in enumerate(goal_names):
     plt.setp(axes[i, 0], ylabel=goal)
 
 # Labeling the datasets
@@ -60,8 +61,8 @@ for i, ds in enumerate(df_names):
     plt.setp(axes[0, i], title=ds)
 
 fig.set_tight_layout(True)
-fig.suptitle('Fairness vs. discrimination')
-fig.supxlabel('Maximum difference in fairness between any 2 groups')
-fig.supylabel('Brier score')
+#fig.suptitle('Fairness vs. discrimination')
+fig.supxlabel('Maximum Difference in Fairness')
+fig.supylabel('Brier Score')
 
 plt.show()
